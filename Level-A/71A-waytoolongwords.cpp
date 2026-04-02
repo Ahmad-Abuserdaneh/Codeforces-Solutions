dd1cebefca40fff2605adfcd3a7b405e180cef97
Ahmad Abuserdaneh <ahmadabuserdaneh0@gmail.com>
2026-04-02 19:38:20 +0300

    Update 71A-waytoolongwords.cpp

--- Level-A/71A-waytoolongwords.cpp
+++ Level-A/71A-waytoolongwords.cpp
@@ -1,3 +1,12 @@
+/*
+problemset: https://codeforces.com/problemset/problem/71/A
+discription: this is a simple code, in this code I made a for loop that loops as many times as the user inputs and
+makes him enter a word for every loop, the question asks that the word should be over 10 leters for the condition to 
+be done, so in my code if the word is over 10 letters it has to be printed as (letter number letter) if not over 10 it
+just prints the word normaly.
+*/
+
+
 
 #include <iostream>
 #include <cctype>
